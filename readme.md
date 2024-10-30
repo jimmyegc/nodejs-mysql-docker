8ecd75c8c560f38e3c50dfa448cfc40f115cb577
Jimmy García <jimmyegc@gmail.com>
2024-10-29 20:21:12 -0600

    fix 10292024

---
+++ readme.md
@@ -0,0 +1,4 @@
+### Construir el Contenedor Dockerfile
+- docker build -t nodemysql .
+
+[Docker Compose con Nodejs y MySQL Tutorial](https://www.youtube.com/watch?v=Aj8E-Vhs1VM)
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import { createPool } from 'mysql2/promise'
+import { config } from 'dotenv'
+config()
+
+const app = express() 
+
+console.log({
+  host: process.env.MYSQLDB_HOST,
+  password: process.env.MYSQLDB_ROOT_PASSWORD,
+  port: process.env.MYSQLDB_DOCKER_PORT,
+})
+
+const pool = createPool({
+  host: process.env.MYSQLDB_HOST,
+  database: process.env.MYSQLDB_DATABASE,
+  user: 'root',
+  password: process.env.MYSQLDB_ROOT_PASSWORD,
+  port: process.env.MYSQLDB_DOCKER_PORT
+})
+
+app.get("/", (req,res) => {
+  res.send({ message: 'hello' })
+})
+
+app.get("/ping", async (req,res) => {
+  const result = await pool.query('SELECT NOW()');
+  res.json(result[0])
+})
+
+app.listen(process.env.NODE_DOCKER_PORT)
+console.log("Server on port", process.env.NODE_DOCKER_PORT)
